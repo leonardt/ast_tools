ec1412261e706a0283c3139376dd01fea9b182e9
Caleb Donovick <donovick@cs.stanford.edu>
2019-08-15 10:19:05 -0700

    Make single_return more robust

--- ast_tools/passes/single_return.py
+++ ast_tools/passes/single_return.py
@@ -1,6 +1,7 @@
 import ast
 from itertools import count
 import warnings
+import weakref
 import typing as tp
 
 import astor
@@ -9,6 +10,8 @@ from . import Pass
 from . import _PASS_ARGS_T
 from ast_tools.stack import SymbolTable
 
+__ALL__ = ['single_return']
+
 
 class ReturnMover(ast.NodeTransformer):
     """
@@ -16,73 +19,47 @@ class ReturnMover(ast.NodeTransformer):
     """
 
     def __init__(self,
-            return_value_prefx: str = '__return_value',
-            use_bool_ops: bool = True, #controls whether to use not, and or ~, &
+            return_value_prefx: str,
             ):
-        self.cond_stack = []
-        self.returns = []
+        self.returns = self.ptr = []
         self.r_val_idx = count()
         self.return_value_prefx = return_value_prefx
-        self.use_bool_ops = use_bool_ops
-        if use_bool_ops:
-            self.not_ = ast.Not
-        else:
-            self.not_ = ast.Invert
-        self.root = None
-
-    def visit(self, node: ast.FunctionDef):
-        if self.root is None:
-            if not isinstance(node, ast.FunctionDef):
-                raise TypeError(f'ReturnMover expects a FunctionDef not a {type(node)}')
-            self.root = node
-            func = self.generic_visit(node)
-            return func
-
-        else:
-            return super().visit(node)
 
     def visit_If(self, node: ast.If):
         test = node.test
-        body = []
-        orelse = []
+        ptr = self.ptr
+        ptr.append(ast.If(test, [], []))
 
-        self.cond_stack.append(test)
+        self.ptr = ptr[-1].body
+        body = []
         for child in node.body:
             child = self.visit(child)
             if child is not None:
                 body.append(child)
 
-        self.cond_stack.pop()
-        self.cond_stack.append(ast.UnaryOp(op=self.not_(), operand=test))
+        self.ptr = ptr[-1].orelse
+        orelse = []
         for child in node.orelse:
             child = self.visit(child)
             if child is not None:
                 orelse.append(child)
 
-        self.cond_stack.pop()
+        self.ptr = ptr
         return ast.If(test=test, body=body, orelse=orelse)
 
     def visit_Return(self, node: ast.Return):
         r_val = node.value
         r_name = self.return_value_prefx + str(next(self.r_val_idx))
-        self.returns.append((self._reduce_cond_stack(), r_name))
+        self.ptr.append(
+            ast.Assign(
+                targets=[ast.Name(id=self.return_value_prefx, ctx=ast.Store())],
+                value=ast.Name(id=r_name, ctx=ast.Load())
+            )
+        )
         return ast.Assign(
             targets=[ast.Name(r_name, ast.Store())],
             value=r_val,
         )
-
-    def _reduce_cond_stack(self) -> ast.expr:
-        if len(self.cond_stack) == 0:
-            return ast.NameConstant(True)
-        elif len(self.cond_stack) == 1:
-            return self.cond_stack[0]
-        elif self.use_bool_ops:
-            return ast.BoolOp(ast.And(), list(self.cond_stack))
-        else:
-            cond = self.cond_stack[0]
-            for c in self.cond_stack[1:]:
-                cond = ast.BoolOp(cond, ast.BitAnd(), c)
-            return cond
 
     # don't recurs into defs
     def visit_ClassDef(self, node: ast.ClassDef):
@@ -94,34 +71,35 @@ class ReturnMover(ast.NodeTransformer):
     def visit_AsyncFunctionDef(self, node: ast.AsyncFunctionDef):
         return node
 
-class single_return(Pass):
-    def rewrite(self, tree, env):
-        visitor = ReturnMover()
-        tree = visitor.visit(tree)
-        if len(visitor.returns) == 0:
-            return tree
-        orelse = None
-        for cond, r_val in reversed(visitor.returns):
-            if_ = ast.If(
-                test=cond,
-                body=[
-                    ast.Assign(
-                        targets=[ast.Name(id=visitor.return_value_prefx, ctx=ast.Store())],
-                        value=ast.Name(id=r_val, ctx=ast.Load())
-                    )
-                ]
-            )
-            if orelse is None:
-                if_.orelse = []
-            else:
-                if_.orelse = [orelse]
-            orelse = if_
+def _prove_return(body: tp.Sequence[ast.stmt]):
+    for stmt in body:
+        if isinstance(stmt, ast.Return):
+            return True
+        elif isinstance(stmt, ast.If):
+            if _prove_return(stmt.body) and _prove_return(stmt.orelse):
+                return True
+    return False
 
-        assert orelse is not None
-        tree.body.append(orelse)
+class single_return(Pass):
+    def __init__(self, prove_returns: bool = False):
+        self.prove_returns = prove_returns
+
+    def rewrite(self, tree: ast.FunctionDef, env: SymbolTable):
+        if not isinstance(tree, ast.FunctionDef):
+            raise TypeError('single_return should only be applied to functions')
+        if not _prove_return(tree.body):
+            if self.prove_returns:
+                raise SyntaxError(f'Cannot prove that {tree.name} returns')
+            else:
+                warnings.warn(f'Cannot prove that {tree.name} returns')
+
+        r_name = '__return_value'
+        visitor = ReturnMover(r_name)
+        tree = visitor.visit(tree)
+        tree.body.extend(visitor.returns)
         tree.body.append(
             ast.Return(
-                value=ast.Name(id=visitor.return_value_prefx, ctx=ast.Load())
+                ast.Name(id=r_name, ctx=ast.Load())
             )
         )
         return tree, env
