a5ec0907e1c529a7208b15cd27366fe9387de7eb
Caleb Donovick <donovick@cs.stanford.edu>
2019-08-23 13:09:09 -0700

    Add tests to make coverage happy

--- tests/test_common.py
+++ tests/test_common.py
@@ -1,7 +1,8 @@
 import ast
 import astor
 
-from ast_tools.common import get_ast, gen_free_name, SymbolTable
+from ast_tools.common import get_ast, gen_free_name, gen_free_prefix
+from ast_tools.stack import SymbolTable
 from ast_tools.passes import begin_rewrite, end_rewrite
 
 
@@ -13,7 +14,7 @@ def test_get_ast():
     assert ast_str_0 == ast_str_1
 
 
-def test_gen_free():
+def test_gen_free_name():
     src = '''
 class P:
     P5 = 1
@@ -24,6 +25,10 @@ P1 = P0()
 '''
     tree = ast.parse(src)
     env = SymbolTable({}, {})
+
+    free_name = gen_free_name(tree, env)
+    assert free_name == '__auto_name_0'
+
     free_name = gen_free_name(tree, env, prefix='P')
     assert free_name == 'P2'
     env = SymbolTable({'P3': 'foo'}, {})
@@ -33,6 +38,23 @@ P1 = P0()
     free_name = gen_free_name(tree, env, prefix='P')
     assert free_name == 'P4'
 
+def test_gen_free_prefix():
+    src = '''
+class P:
+    P5 = 1
+    def __init__(self): self.y = 0
+def P0():
+    return P.P5
+P1 = P0()
+'''
+    tree = ast.parse(src)
+    env = SymbolTable({}, {})
+
+    free_prefix = gen_free_prefix(tree, env)
+    assert free_prefix == '__auto_prefix_0'
+
+    free_prefix = gen_free_prefix(tree, env, 'P')
+    assert free_prefix == 'P2'
 
 def test_exec_in_file():
     x = 3
