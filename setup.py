f6fa2d3ea18b16fb10c6b34c46f935be98ace960
Lenny Truong <leonardtruong@fb.com>
2019-09-03 12:28:23 -0700

    Test travis release

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     url='https://github.com/leonardt/ast_tools',
     author='Leonard Truong',
     author_email='lenny@cs.stanford.edu',
-    version='0.0.7',
+    version='0.0.8',
     description='Toolbox for working with the Python AST',
     scripts=[],
     packages=[
