cb1cd27c5283186a8b7e6018702cc7ba1a3ae50a
Lenny Truong <leonardtruong@fb.com>
2020-03-02 10:13:33 -0800

    Release 0.0.14

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     url='https://github.com/leonardt/ast_tools',
     author='Leonard Truong',
     author_email='lenny@cs.stanford.edu',
-    version='0.0.13',
+    version='0.0.14',
     description='Toolbox for working with the Python AST',
     scripts=[],
     packages=[
