f360c915c7bf3c1af43180917983245b785106a9
Caleb Donovick <donovick@cs.stanford.edu>
2021-02-08 15:35:22 -0800

    fix setup and step version

--- setup.py
+++ setup.py
@@ -52,14 +52,15 @@ setup(
     url='https://github.com/leonardt/ast_tools',
     author='Leonard Truong',
     author_email='lenny@cs.stanford.edu',
-    version='0.1.0',
+    version='0.1.1',
     description='Toolbox for working with the Python AST',
     scripts=[],
     packages=[
         f'{PACKAGE_NAME}',
-        f'{PACKAGE_NAME}.visitors',
+        f'{PACKAGE_NAME}.cst_utils'
+        f'{PACKAGE_NAME}.passes',
         f'{PACKAGE_NAME}.transformers',
-        f'{PACKAGE_NAME}.passes'
+        f'{PACKAGE_NAME}.visitors',
     ],
     install_requires=['astor', 'libcst'],
     long_description=LONG_DESCRIPTION,
