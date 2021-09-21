94feb338b944a839a758947bdbab62ea3b9d83da
Caleb Donovick <donovick@cs.stanford.edu>
2021-09-21 13:53:57 -0700

    Add fix

--- ast_tools/transformers/if_inliner.py
+++ ast_tools/transformers/if_inliner.py
@@ -11,6 +11,12 @@ class Inliner(cst.CSTTransformer):
         super().__init__()
         self.env = env
 
+    def visit_If(self, node):
+        # Control recursion order.
+        # Need to avoid putting an a flatten sentinel in orelse
+        # can happen if the elif is being inlined
+        return False
+
     def leave_If(
             self,
             original_node: cst.If,
@@ -23,16 +29,27 @@ class Inliner(cst.CSTTransformer):
             is_constant = False
         if is_constant and isinstance(cond_obj, inline):
             if cond_obj:
-                updated_node = cst.FlattenSentinel(updated_node.body.body)
+                new_body = updated_node.body.visit(self)
+                updated_node = cst.FlattenSentinel(new_body.body)
             else:
                 orelse = updated_node.orelse
                 if orelse is None:
                     updated_node = cst.RemoveFromParent()
                 elif isinstance(orelse, cst.If):
-                    updated_node = updated_node.orelse
+                    updated_node = updated_node.orelse.visit(self)
                 else:
                     assert isinstance(orelse, cst.Else)
-                    updated_node = cst.FlattenSentinel(orelse.body.body)
+                    new_body = updated_node.orelse.body.visit(self)
+                    updated_node = cst.FlattenSentinel(new_body.body)
+        else:
+            new_body = updated_node.body.visit(self)
+            if updated_node.orelse:
+                new_orelse = updated_node.orelse.visit(self)
+            else:
+                new_orelse = None
+
+            updated_node = updated_node.with_changes(body=new_body, orelse=new_orelse)
+
         return super().leave_If(original_node, updated_node)
 
 
